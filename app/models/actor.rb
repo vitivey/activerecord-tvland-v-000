a492e151403464f9691551d6334489e920a4cd90
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-16 02:04:20 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.map do |character|
-      puts "#{character.name} - #{character.show.name}"
+      "#{character.name} - #{character.show.name}"
     end
   end
 
