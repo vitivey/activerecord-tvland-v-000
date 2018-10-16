1b7c4850ece2578b6db0c2f23d37d5fc80b92050
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-16 01:57:51 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 6) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "show_id"
+    t.integer "actor_id"
+    t.string "catchphrase"
+  end
+
+  create_table "networks", force: :cascade do |t|
+    t.string "call_letters"
+    t.integer "channel"
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "network_id"
+  end
 
 end
